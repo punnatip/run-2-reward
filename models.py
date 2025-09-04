13b9f70aa2f88220aaf763e49e88e2b589690007
Punnatip <jubujob2532@gmail.com>
2025-09-04 23:35:58 +0700

    fix: main.py, schemas.py

--- models.py
+++ models.py
@@ -3,12 +3,7 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
-# กำหนดโครงสร้างตาราง rewards โดยการสร้าง Class ที่สืบทอดจาก SQLModel
 class Reward(SQLModel, table=True):
-    # __tablename__ จะถูกสร้างจากชื่อคลาสโดยอัตโนมัติ (เป็น "reward")
-    # หากต้องการชื่ออื่น ให้กำหนด __tablename__ = "rewards" ที่นี่ได้
-
-    # กำหนดคอลัมน์ต่างๆ โดยใช้ Type Annotation และ Field
-    id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    name: str = Field(index=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
     quantity: int
